39383fbf4ca6c7ca0d1819fdb99ca0111161e848
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-21 17:52:19 +0100

    Mean value

---
+++ main.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrongnum = 36
+correctnum = 56
+total= 40
+#sum
+sum = mean1*total
+print("The added val is:",sum)
+#correct sum val
+num2 = sum -((wrongnum)-(correctnum))
+print("New number :",num2)
+#correct avg
+mean2 = num2/total
+print("correct mean:",mean2)
